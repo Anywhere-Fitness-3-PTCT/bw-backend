2a591817e159fdd0884fdc3b1a18292bbbb9c525
stkirk <shaanekirk@gmail.com>
2021-11-18 15:32:53 -0500

    restricted middleware checks for token on users route

---
+++ api/server-middleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const { TOKEN_SECRET } = require("../config/secrets");
+
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        next({ status: 401, message: "token invalid" });
+      } else {
+        req.decodedJwt = decoded;
+        next();
+      }
+    });
+  } else {
+    next({ status: 401, message: "token required" });
+  }
+};
+
+module.exports = {
+  restricted,
+};
--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const usersRouter = require("./users/users-router");
 const classesRouter = require("./classes/classes-router");
 const authRouter = require("./auth/auth-router");
+const { restricted } = require("./server-middleware");
 
 const server = express();
 server.use(express.json());
@@ -11,7 +12,7 @@ server.use(helmet());
 server.use(cors());
 
 server.use("/api/classes", classesRouter);
-server.use("/api/users", usersRouter);
+server.use("/api/users", restricted, usersRouter);
 server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
