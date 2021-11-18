75cf984d67654d7f50d633109624e6d0ba527236
stkirk <shaanekirk@gmail.com>
2021-11-18 14:07:29 -0500

    validate payload middleware for login/register added

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,1 +1,13 @@
+const Users = require("../users/users-model");
+
+const validateUserPayload = (req, res, next) => {
+  if (!req.body.username || !req.body.password) {
+    next({ status: 422, message: "username and password required" });
+  } else next();
+};
+
 //when validating user exists upon login make sure to include user_id, username, and role when attaching user to req object in findBy model function
+
+module.exports = {
+  validateUserPayload,
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const generateToken = require("../../utils/generateToken");
+const Users = require("../users/users-model");
+const { validateUserPayload } = require("./auth-middleware");
 
 const router = express.Router();
 
-router.post("/register", (req, res, next) => {
+router.post("/register", validateUserPayload, (req, res, next) => {
   res.send("[POST] to auth/register");
 });
 
-router.post("/login", (req, res, next) => {
+router.post("/login", validateUserPayload, (req, res, next) => {
   res.send("[POST] to auth/login");
 });
 
