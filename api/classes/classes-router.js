4f94c6a7b11b4ba20f9fdf193bd8365a6276841e
stkirk <shaanekirk@gmail.com>
2021-11-19 11:37:13 -0500

    scaffolded classes CRUD endpoints

--- api/classes/classes-router.js
+++ api/classes/classes-router.js
@@ -6,4 +6,20 @@ router.get("/", (req, res, next) => {
   res.json("[GET] all classes");
 });
 
+router.get("/:class_id", (req, res, next) => {
+  res.json("[GET] class by class_id");
+});
+
+router.post("/", (req, res, next) => {
+  res.json("[POST] new classes");
+});
+
+router.put("/:class_id", (req, res, next) => {
+  res.json("[PUT] class");
+});
+
+router.delete("/:class_id", (req, res, next) => {
+  res.json("[DELETE] class by id");
+});
+
 module.exports = router;
